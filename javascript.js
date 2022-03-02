5a8278cef8e0db2e4c068c40f935d89c75221961
Jake Lyell <jakelyell7@gmail.com>
2022-03-02 21:07:44 +1000

    Works for one round, needs 5 then final winner logged.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,19 @@
+//Global variables which apply to any function.
 const choices = ['rock', 'paper', 'scissors'];
 const playerSelection = userPlay();
 const computerSelection = computerPlay();
+const draw = ("It's a Draw!");
+const playerWin = ("You Win!");
+const computerWin = ("Computer Wins!");
+let playerScore = 0;
+let computerScore = 0;
 
 //loops five rounds of the game
 function game() {
-    for (let i = 0; i <=5; i++) {
+    for (let i = 1; i <= 5; i++) {
         playRound();
     }
-    console.log('You chose ' + playerSelection + '.');
-    console.log('Computer chose ' + computerSelection + '.');
-    console.log(playRound(playerSelection, computerSelection));
+    roundResult();
 }
 
 //computer makes random choice. 
@@ -18,22 +22,35 @@ function computerPlay() {
 }
 
 //user selects from options.
-function userPlay () {
-    return prompt("Select Scissors, Paper or Rock.")
+function userPlay() {
+    return prompt ("Select Scissors, Paper or Rock.").toLowerCase();
     
 }
 
 //single round is played.
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return 'It\'s a Tie!' 
+        return draw 
 } else if (playerSelection === 'rock' && computerSelection === 'scissors' || 
            playerSelection === 'scissors' && computerSelection === 'paper' ||
            playerSelection === ' paper' && computerSelection === 'rock') {
-        return 'You Win!!'
+        playerScore++;
+        return playerWin;
 } else {
-    return 'Computer Wins!';
+    computerScore++;
+    return computerWin;
 }
+}
+
+//Result of the round is displayed in the console.
+function roundResult() {
+    console.log('You chose ' + playerSelection + '.');
+    console.log('Computer chose ' + computerSelection + '.');
+    console.log(playRound(playerSelection, computerSelection));
+    console.log('Your score: ' + playerScore);
+    console.log('Computer score: ' + computerScore);
+    console.log('Draws: ');
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 }
 
 
