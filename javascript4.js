9dbd9ae704e08e091a10cfc7e12891779bc371ce
Jake Lyell <jakelyell7@gmail.com>
2022-03-08 00:33:38 +1000

    js4 adjustment

--- javascript4.js
+++ javascript4.js
@@ -2,24 +2,31 @@ const options = ['rock', 'paper', 'scissors'];
 const win = 'You win!';
 const loss = 'You lose!';
 const tie = 'It\'s a tie!';
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
-    const choice = options[Math.floor(Math.random() * options.length)];
-    return choice;
+    return options[Math.floor(Math.random() * options.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection == 'scissors') {
+        playerScore++;
         return win;
     } else if (playerSelection === 'rock' && computerSelection == 'paper') {
+        computerScore++;
         return loss;
     } else if (playerSelection === 'paper' && computerSelection == 'rock') {
+        playerScore++;
         return win; 
     } else if (playerSelection === 'paper' && computerSelection == 'scissors') {
+        computerScore++;
         return loss;
     } else if (playerSelection === 'scissors' && computerSelection == 'paper') {
+        playerScore++;
         return win;
     } else if (playerSelection === 'scissors' && computerSelection == 'rock') {
+        computerScore++;
         return loss;
     } else {
         return tie;
@@ -27,14 +34,16 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-    for (let i = 5; i <=5; i++) {
     playRound();
 }
-}
+
+const playerSelection = prompt('Enter rock, paper or scissors.');
+const computerSelection = computerPlay();
+console.log('You chose ' + playerSelection + '.');
+console.log('Computer chose ' + computerSelection + '.');
+console.log(playRound(playerSelection, computerSelection));
+console.log('Your score: ' + playerScore);
+console.log('Computer score: ' + computerScore);
+console.log('------------------------------------');
 
 game();
-const playerSelection = prompt('Rock, paper, scissors?');
-const computerSelection = computerPlay();
-console.log('You chose ' + playerSelection);
-console.log('Computer chose ' + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
