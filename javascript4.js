179473e44ad10385ee82da1103757862f41ed605
Jake Lyell <jakelyell7@gmail.com>
2022-03-09 15:43:06 +1000

    Reallocated certain functions to a for loop after reading on loops.

--- javascript4.js
+++ javascript4.js
@@ -1,20 +1,15 @@
-const options = ['rock', 'paper', 'scissors'];
-const win = 'You win!';
-const loss = 'You lose!';
-const tie = 'It\'s a tie!';
 let playerScore = 0;
 let computerScore = 0;
 
 function computerPlay() {
+    const options = ['rock', 'paper', 'scissors'];
     return options[Math.floor(Math.random() * options.length)];
 }
 
-function userPlay() {
-    let playerChoice= prompt('Choose rock, paper or scissors');
-    return playerChoice;
-}
-
 function playRound(playerSelection, computerSelection) {
+    const win = 'You win!';
+    const loss = 'You lose!';
+    const tie = 'It\'s a tie!';
     if (playerSelection === 'rock' && computerSelection == 'scissors') {
         playerScore++;
         return win;
@@ -38,21 +33,13 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    playRound();
-    logResults();
-}
-
-const playerSelection = userPlay();
-const computerSelection = computerPlay();
-
-function logResults() {
-console.log('You chose ' + playerSelection + '.');
-console.log('Computer chose ' + computerSelection + '.');
-console.log(playRound(playerSelection, computerSelection));
-console.log('Your score: ' + playerScore);
-console.log('Computer score: ' + computerScore);
-console.log('------------------------------------');
-}
-
-game();
+for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Choose rock, paper or scissors");
+    const computerSelection = computerPlay()
+    console.log('You chose ' + playerSelection + '.');
+    console.log('Computer chose ' + computerSelection + '.');
+    console.log(playRound(playerSelection, computerSelection))
+    console.log('Your score: ' + playerScore);
+    console.log('Computer score: ' + computerScore);
+    console.log('------------------------------------');
+  }
