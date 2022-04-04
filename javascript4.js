0cb8c55f1badb1b40e3a41986e979bce1b253623
Jake Lyell <jakelyell7@gmail.com>
2022-04-04 14:17:02 +1000

    Plays first to 5 wins in console. Confirmation box alerts winner and restarts the game

--- javascript4.js
+++ javascript4.js
@@ -1,23 +1,38 @@
 let playerScore = 0;
 let computerScore = 0;
-
-//This function plays a round and displays results in console every time a button is clicked. 
 const buttons = document.querySelectorAll('button'); 
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        playRound(), game();
-    })
-});
 
+//computer choice
 function computerPlay() {
     const options = ['rock', 'paper', 'scissors'];
     return options[Math.floor(Math.random() * options.length)];
 }
 
+//Player chooses and round is played
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerSelection = button.id;
+        let computerSelection = computerPlay();
+        
+            console.log('You chose ' + playerSelection + '.');
+            console.log('Computer chose ' + computerSelection + '.');
+            console.log(playRound(playerSelection, computerSelection))
+            console.log('Your score: ' + playerScore);
+            console.log('Computer score: ' + computerScore);
+            console.log('------------------------------------');
+        //Ends game when score of 5 is reached
+        if (computerScore == 5 || playerScore == 5) {
+            return finalResult();
+        }
+    })
+});
+
+
+
 function playRound(playerSelection, computerSelection) {
-    const win = 'You win!';
-    const loss = 'You lose!';
-    const tie = 'It\'s a tie!';
+    const win = 'Round won!';
+    const loss = 'Round lost!';
+    const tie = 'Round tied!';
     if (playerSelection === 'rock' && computerSelection == 'scissors') {
         playerScore++;
         return win;
@@ -39,39 +54,14 @@ function playRound(playerSelection, computerSelection) {
     } else {
         return tie;
     }
-    
-}
-
-
-//this function defines the player and computer choices, then displays final results after a score reaches 5.
-function game() {
-//for (let i = 0; i < 5; i++) {
-    let playerSelection = buttons.id; //not sure how to get button id to be playerSelection variable.
-    const computerSelection = computerPlay();
-    console.log('You chose ' + playerSelection + '.');
-    console.log('Computer chose ' + computerSelection + '.');
-    console.log(playRound(playerSelection, computerSelection))
-    console.log('Your score: ' + playerScore);
-    console.log('Computer score: ' + computerScore);
-    console.log('------------------------------------');
-    if (computerScore == 5 || playerScore == 5) {
-        return finalResult();
-    }
 }
 
   function finalResult() {
-    console.log('Final Results:')
     if (playerScore > computerScore) {
-        console.log('You scored: ' + playerScore);
-        console.log('Computer scored: ' + computerScore);
-        console.log('You win the game!');
-    } else if (playerScore < computerScore) {
-        console.log('You scored: ' + playerScore);
-        console.log('Computer scored: ' + computerScore);
-        console.log('Computer wins the game!');
+        confirm('You win the game! Play again?');
+        location.reload();
     } else {
-        console.log('You scored: ' + playerScore);
-        console.log('Computer scored: ' + computerScore);
-        console.log('Its a tie!');
+        confirm('Computer wins the game! Play again?')
+        location.reload();
     }
 }
