b45981e8602cdb44722720b1d4bfae4ec1ea9e09
Jake Lyell <jakelyell7@gmail.com>
2022-04-04 21:29:18 +1000

    Adding animations to buttons

--- javascript4.js
+++ javascript4.js
@@ -1,16 +1,10 @@
 let playerScore = 0;
 let computerScore = 0;
-const buttons = document.querySelectorAll('button'); 
+const buttons = document.querySelectorAll('button');
 
-//computer choice
-function computerPlay() {
-    const options = ['rock', 'paper', 'scissors'];
-    return options[Math.floor(Math.random() * options.length)];
-}
-
-//Player chooses and round is played
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
+        keyPress();
         playRound(playerSelection = button.id, computerSelection = computerPlay());
         let pScore = playerScore;
         let cScore = computerScore;
@@ -20,7 +14,6 @@ buttons.forEach((button) => {
         document.getElementById('computerChoice').innerHTML = 'Computer chose ' + cChoice;
         document.getElementById('playerResult').innerHTML = 'Your score: ' + pScore;
         document.getElementById('computerResult').innerHTML = 'Computer score: ' + cScore;
-    
         //Ends game when score of 5 is reached
         if (computerScore == 5 || playerScore == 5) {
             return finalResult();
@@ -28,42 +21,35 @@ buttons.forEach((button) => {
     })
 });
 
-
+function computerPlay() {
+    const options = ['rock', 'paper', 'scissors'];
+    return options[Math.floor(Math.random() * options.length)];
+}
 
 function playRound(playerSelection, computerSelection) {
-    const win = 'Round won!';
-    const loss = 'Round lost!';
-    const tie = 'Round tied!';
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
         playerScore++;
-        return win;
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
         computerScore++;
-        return loss;
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
         playerScore++;
-        return win; 
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
         computerScore++;
-        return loss;
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
         playerScore++;
-        return win;
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
         computerScore++;
-        return loss;
     } else {
-        return tie;
+        return
     }
 }
 
-  function finalResult() {
+function finalResult() {
     if (playerScore > computerScore) {
         document.getElementById('finalResult').innerHTML = 'You Win!';
         document.getElementById('replay').innerText = 'Choose Rock, Paper or Scissors to play again';
         playerScore = 0;
         computerScore = 0;
-        
     } else {
         document.getElementById('finalResult').innerHTML = 'Computer Wins!';
         document.getElementById('replay').innerText = 'Choose Rock, Paper or Scissors to play again';
@@ -71,3 +57,17 @@ function playRound(playerSelection, computerSelection) {
         computerScore = 0;
     }
 }
+
+// Trying to animate the buttons.
+function keyPress() {
+    const key = document.querySelector('button');
+    key.classList.add('keypress');
+}
+
+function unPress(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('keypress');
+}
+
+const press = document.querySelectorAll('.btn');
+press.forEach(key => key.addEventListener('transitionend', unPress));
